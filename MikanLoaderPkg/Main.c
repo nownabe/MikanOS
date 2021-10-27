b1541da6195ca055bca40a609bd8c916230e5955
nownabe <nownabe@users.noreply.github.com>
2021-10-27 23:35:46 +0900

    2.5 メモリマップの取得
    Memo: #3

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,5 +1,39 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/PrintLib.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <Protocol/DiskIo2.h>
+#include <Protocol/BlockIo.h>
+
+// #@@range_begin(struct_memory_map)
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID* buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+// #@@range_end(struct_memory_map)
+
+// #@@range_begin(get_memory_map)
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  map->map_size = map->buffer_size;
+
+  // gBS: global Boot Service
+  return gBS->GetMemoryMap(&map->map_size,
+                           (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+                           &map->map_key,
+                           &map->descriptor_size,
+                           &map->descriptor_version);
+}
+// #@@range_end(get_memory_map)
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
